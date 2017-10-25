9257a91e766c07f1b16da19ff8ce62a6fb93ad6b
GabrielTrev <micrumba@yahoo.com.br>
2017-10-25 14:14:35 -0200

    Adicionada documentação ao código

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,12 +35,14 @@ import static org.lwjgl.opengles.GLES20.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
-
+// Estrutura contendo vertice do poligono a ser desenhado
 class Vertex {
     public double x;
     public double y;
 }
 
+// Estrutura basica de um no da ET
+// Contem o y_max, o x do y_min e o inverso do coeficiente angular da reta
 class ETNode{
     private Double maxY;
     private Double xMinY;
@@ -48,12 +50,14 @@ class ETNode{
 
     public ETNode() {}
 
+    // Inicializacao do ETNode, dado um par de vertices
     public ETNode(Vertex v1, Vertex v2) {
         this.maxY = Math.max(v1.y, v2.y);
         this.xMinY = (v1.y < v2.y) ? v1.x : v2.x;
         this.inverseSlope = (v2.x - v1.x) / (v2.y - v1.y);
     }
 
+    // Inicializacao do ETNode, de acordo com os valores de seus atributos
     public ETNode(Double maxY, Double xMinY, Double inverseSlope) {
         this.maxY = maxY;
         this.xMinY = xMinY;
@@ -69,6 +73,7 @@ class ETNode{
 
 }
 
+// Comparator utilizado para o ETNode para ordenacao dos nos na tabela
 class ETNodeComparator implements Comparator<ETNode> {
     @Override
     public int compare(ETNode node1, ETNode node2) {
@@ -80,7 +85,10 @@ class ETNodeComparator implements Comparator<ETNode> {
 // https://www.lwjgl.org/guide
 public class Main {
 
+    // Lista de vertices do poligono a ser desenhado
     private ArrayList<Vertex> verticesList = new ArrayList<>();
+
+    // Variavel de condicao que indica se poligono foi fechado
     private boolean closedPolygon = false;
 
     // The window handle
@@ -89,6 +97,7 @@ public class Main {
     private static int width = 800;
     private static int height = 600;
 
+    // Implementacao do algoritmo de preenchimento de poligonos, dado o conjunto de vertices
     public void scanLine(ArrayList<Vertex> vertices) {
         if (!closedPolygon) { return; }
 
@@ -101,7 +110,9 @@ public class Main {
 
         // Preenchimento da ET
         for (int i = 0; i < verticesList.size() - 1; i++) {
+            // No criado de acordo com o proximo par de vertices
             ETNode node = new ETNode(verticesList.get(i), verticesList.get(i + 1));
+            // Indice do no na tabela corresponde ao valor de y_min
             double indice = Math.min(verticesList.get(i).y, verticesList.get(i + 1).y);
             ET.get((int) indice).add(node);
         }
@@ -123,11 +134,14 @@ public class Main {
             }
         }
 
+        // Atualizacao dos valores das linhas da AET e preenchimento das linhas
         for (int i = indice; i < height - 1; i++) {
+            // Insercaos dos nos presentes na linha da ET da mesma linha da AET
             for (ETNode node : ET.get(i)) {
                 AET.get(i).add(node);
             }
 
+            // Atualizacao dos valores do nivel anterior da AET
             for (ETNode node : AET.get(i)) {
                 ETNode nodeAux = new ETNode(node.getMaxY(), node.getxMinY(), node.getInverseSlope());
                 nodeAux.update();
@@ -135,9 +149,11 @@ public class Main {
             }
         }
 
+        // Ordenacao dos nos da linha da AET
         for (int i = 0; i < height; i++) {
             Collections.sort(AET.get(i), new ETNodeComparator());
 
+            // Desenhos dos pontos, de acordo com os pares de valores de x do y_min
             for (int j = 0; j < AET.get(i).size() - 1; j += 2) {
                 for (int k = (int) (double) AET.get(i).get(j).getxMinY(); k < (int) (double) AET.get(i).get(j + 1).getxMinY(); k++) {
                     glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
@@ -167,10 +183,19 @@ public class Main {
 
     }
 
+    // Operacao de translacao do poligono
+    // Parametros de entrada:
+    //      list: lista de vertices do poligono
+    //      direction: direcao de translacao
+    //          0: para baixo
+    //          1: para cima
+    //          2: para a esquerda
+    //          3: para a direita
+    //      value: numero de pixels que devem ser transladados
     public void translation(ArrayList<Vertex> list, int direction, double value) {
 
         switch (direction) {
-            // Up
+            // Translacao para cima (sentido negativo de y)
             case 0:
                 for(int i = 0; i < list.size(); i++) {
                     Vertex aux = new Vertex();
@@ -180,7 +205,7 @@ public class Main {
                 }
                 break;
 
-            // Down
+            // Translacao para baixo (sentido positivo de y)
             case 1:
                 for(int i = 0; i < list.size(); i++) {
                     Vertex aux = new Vertex();
@@ -190,7 +215,7 @@ public class Main {
                 }
                 break;
 
-            // Left
+            // Translacao para a esquerda (sentido negativo de x)
             case 2:
                 for(int i = 0; i < list.size(); i++) {
                     Vertex aux = new Vertex();
@@ -200,7 +225,7 @@ public class Main {
                 }
                 break;
 
-            // Right
+            // Translacao para a direita (sentido positivo de x)
             case 3:
                 for(int i = 0; i < list.size(); i++) {
                     Vertex aux = new Vertex();
@@ -215,49 +240,73 @@ public class Main {
         }
     }
 
+    // Operacao de escalonamento do poligono
+    // Parametros de entrada:
+    //      originalList: lista de vertices do poligono
+    //      direction: direcao do escalonamento
+    //          0: escalonamento em x
+    //          1: escalonamento em y
+    //      value: numero de pixels do escalonamento
     public void scale(ArrayList<Vertex> originalList, int direction, double value) {
 
         ArrayList<Vertex> auxList = new ArrayList<>();
         auxList.addAll(originalList);
 
+        // Translacao do poligono para a origem
         translation(auxList, 0, auxList.get(0).y);
         translation(auxList, 2, auxList.get(0).x);
 
         switch(direction) {
-
+            // 0: escalonamento no eixo x
             case 0:
                 for(int i = 0; i < auxList.size(); i++) {
 
+                    // Operacao de escalonamento sobre o vertice (x,y):
+                    // | x' | =  | v  0 | . | x |
+                    // | y' |    | 0  1 |   | y |
                     double[][] p = { {auxList.get(i).x}, {auxList.get(i).y} };
                     double[][] s = { {value, 0}, {0, 1} };
 
+                    // Multiplicacao da matriz de escalonamento
                     RealMatrix matrixP = MatrixUtils.createRealMatrix(p);
                     RealMatrix matrixS = MatrixUtils.createRealMatrix(s);
                     RealMatrix matrixPLine = matrixS.multiply(matrixP);
 
+                    // Escrita das novas posicoes do veritice
                     Vertex aux = new Vertex();
                     aux.x = matrixPLine.getEntry(0,0);
                     aux.y = matrixPLine.getEntry(1,0);
                     auxList.set(i, aux);
                 }
+
+                // Deslocamento do poligono de volta para a posicao original
                 translation(auxList, 1, originalList.get(0).y);
                 translation(auxList, 3, originalList.get(0).x * value);
                 break;
 
+            // 1: escalonamento no eixo y
             case 1:
                 for(int i = 0; i < auxList.size(); i++) {
+
+                    // Operacao de escalonamento sobre o vertice (x,y):
+                    // | x' | =  | 1  0 | . | x |
+                    // | y' |    | 0  v |   | y |
                     double[][] p = { {auxList.get(i).x}, {auxList.get(i).y} };
                     double[][] s = { {1, 0}, {0, value} };
 
+                    // Multiplicacao da matriz de escalonamento
                     RealMatrix matrixP = MatrixUtils.createRealMatrix(p);
                     RealMatrix matrixS = MatrixUtils.createRealMatrix(s);
                     RealMatrix matrixPLine = matrixS.multiply(matrixP);
 
+                    // Escrita das novas posicoes do veritice
                     Vertex aux = new Vertex();
                     aux.x = matrixPLine.getEntry(0,0);
                     aux.y = matrixPLine.getEntry(1,0);
                     auxList.set(i, aux);
                 }
+
+                // Deslocamento do polígono de volta para a posicao original
                 translation(auxList, 1, originalList.get(0).y * value);
                 translation(auxList, 3, originalList.get(0).x);
                 break;
@@ -270,7 +319,11 @@ public class Main {
 
     }
 
-    //procedimento para a rotacao da figura desenhada em relacao ao ponto (0,0)
+    // Procedimento para a rotacao da figura desenhada em relacao ao ponto (0,0)
+    // Parametros de entrada:
+    //      originalList: lista de vertices do poligono
+    //      direction: sentido de roatacao
+    //      angle: angulo de rotacao
     public void rotate(ArrayList<Vertex> originalList, int direction, double angle) {
 
         ArrayList<Vertex> auxList = new ArrayList<>();
@@ -285,14 +338,18 @@ public class Main {
                     x = auxList.get(i).x;
                     y = auxList.get(i).y;
 
+                    // Operacao de rotacao do vertice (x,y) no sentido anti-horario
+                    // | x' | = |  cos(a) -sen(a) | . |  | x |
+                    // | y' |   |  sen(a)  cos(a) |   |  | y |
                     double[][] p = { {x}, {y} };
                     double[][] r = { {cos(angle), -sin(angle)},
                                      {sin(angle),  cos(angle)} };
-                    //p' = r*p
+                    // MUltiplicacao da matriz de rotacao
                     RealMatrix matrixP = MatrixUtils.createRealMatrix(p);
                     RealMatrix matrixR = MatrixUtils.createRealMatrix(r);
                     RealMatrix matrixPLine = matrixR.multiply((matrixP));
 
+                    // Escrita dos novos valores do vertice
                     Vertex aux = new Vertex();
                     aux.x = matrixPLine.getEntry(0, 0); //row,column
                     aux.y = matrixPLine.getEntry(1, 0);
@@ -306,14 +363,18 @@ public class Main {
                     x = auxList.get(i).x;
                     y = auxList.get(i).y;
 
+                    // Operacao de rotacao do vertice (x,y) no sentido horario
+                    // | x' | = |  cos(a) -sen(a) | . |  | x |
+                    // | y' |   |  sen(a)  cos(a) |   |  | y |
                     double[][] p = { {x}, {y} };
                     double[][] r = { {cos(angle), -sin(angle)},
                             {sin(angle),  cos(angle)} };
-                    //p' = r*p
+                    // Multiplicacao da matriz de rotacao
                     RealMatrix matrixP = MatrixUtils.createRealMatrix(p);
                     RealMatrix matrixR = MatrixUtils.createRealMatrix(r);
                     RealMatrix matrixPLine = matrixR.multiply((matrixP));
 
+                    // Escrita dos novos valores do vertice
                     Vertex aux = new Vertex();
                     aux.x = matrixPLine.getEntry(0, 0); //row,column
                     aux.y = matrixPLine.getEntry(1, 0);
@@ -324,7 +385,7 @@ public class Main {
             default:
                 break;
         }
-        //atualizacao da lista de Vertex
+        // Atualizacao da lista de Vertex
         originalList.clear();
         originalList.addAll(auxList);
 
@@ -358,38 +419,48 @@ public class Main {
 
             if(action == GLFW_RELEASE) {
                 switch (key) {
-
+                    // Tecla esc: sair do programa
                     case GLFW_KEY_ESCAPE:
                         glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
                         break;
 
+                    // Tecla para cima: transladar poligono para cima
                     case GLFW_KEY_UP:
                         translation(this.verticesList, 0, 1.0);
                         break;
 
+                    // Tecla para baixo: transladar poligono para baixo
                     case GLFW_KEY_DOWN:
                         translation(this.verticesList, 1, 1.0);
                         break;
 
+                    // Tecla para a esquerda: transladar poligono para a esquerda
                     case GLFW_KEY_LEFT:
                         translation(this.verticesList, 2, 1.0);
                         break;
 
+                    // Tecla para a direita: transladar poligono para a direita
                     case GLFW_KEY_RIGHT:
                         translation(this.verticesList, 3, 1.0);
                   Vertex aux = new Vertex();
               break;
 
+                    // Tecla '[': expansao em x
                     case GLFW_KEY_LEFT_BRACKET:
                         scale(this.verticesList, 0, 1.1);
                         break;
 
+                    // Tecla '[': expansao em y
                     case GLFW_KEY_RIGHT_BRACKET:
                         scale(this.verticesList, 1, 1.1);
                         break;
+
+                    // Tecla 'r': rotacao no sentido anti-horario
                     case GLFW_KEY_R:
                         rotate(this.verticesList, 0, 0.02);
                         break;
+
+                    // Tecla 't': rotacao no sentido horario
                     case GLFW_KEY_T:
                         rotate(this.verticesList, 1, -0.02);
                         break;
@@ -403,7 +474,8 @@ public class Main {
         glfwSetMouseButtonCallback(window, (window, button, action, mods) -> {
             DoubleBuffer b1 = BufferUtils.createDoubleBuffer(1);
             DoubleBuffer b2 = BufferUtils.createDoubleBuffer(1);
-            // Button is pressed, so this is the start point of the line
+
+            // Botao esquerdo pressionado: adicionar vertice ao poligono
             if(button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS) {
                 // mousePressed = true;
                 glfwGetCursorPos(window, b1, b2);
@@ -412,15 +484,18 @@ public class Main {
                 aux.x = b1.get(0);
                 aux.y = b2.get(0);
 
+                // Caso o poligono tenha sido fechado, limpar lista de vertices do poligono
                 if (closedPolygon) {
                     verticesList.clear();
                     verticesList.add(aux);
                     closedPolygon = false;
                 }
+
+                // Caso contrario, apenas adicionar vertice
                 else { verticesList.add(aux); }
 
             }
-            // Button is released, so this is the end point of the line
+            // Botao direito do mouse: fechar poligono
             else if(button == GLFW_MOUSE_BUTTON_2 && action == GLFW_PRESS) {
                 // mousePressed = false;
                 glfwGetCursorPos(window, b1, b2);
